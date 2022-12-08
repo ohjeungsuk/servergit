d11f9295c3ff2c1bcfafde51f13388316fc2390b
ohjeungsuk <82006403+ohjeungsuk@users.noreply.github.com>
2022-12-08 12:16:35 +0900

    up
    up

---
+++ ProjectALL/TCPip/client/ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+
+namespace Console_Tcp_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string strRecvMsg;
+                string strSendMsg;
+
+                TcpClient sockClient = new TcpClient("127.0.0.1", 9090); //소켓생성,커넥트
+                NetworkStream ns = sockClient.GetStream();
+                StreamReader sr = new StreamReader(ns);
+                StreamWriter sw = new StreamWriter(ns);
+
+                strRecvMsg = sr.ReadLine();         //서버로부터 접속 성공 메시지 수신
+                Console.WriteLine(strRecvMsg);
+
+                while (true)
+                {
+                    strSendMsg = Console.ReadLine();
+
+                    sw.WriteLine(strSendMsg);
+                    sw.Flush();
+                    if (strSendMsg == "exit")
+                    {
+                        break;
+                    }
+                    strRecvMsg = sr.ReadLine();
+                    Console.WriteLine(strRecvMsg);
+                }
+                sr.Close();
+                sw.Close();
+                ns.Close();
+                sockClient.Close();
+
+                Console.WriteLine("접속 종료!");
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
---
+++ ProjectALL/TCPip/server/test/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Console_Tcp_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string strMsg;
+
+                TcpListener sockServer = new TcpListener(IPAddress.Parse("127.0.0.1"), 9090); //IP, Port
+                sockServer.Start();
+                Console.WriteLine("Server 시작! Client 연결 대기중...");
+
+                TcpClient client = sockServer.AcceptTcpClient();//Accept
+                Console.WriteLine("Client 접속성공!");
+
+                NetworkStream ns = client.GetStream();
+                StreamReader sr = new StreamReader(ns);
+                StreamWriter sw = new StreamWriter(ns);
+
+                string welcome = "Server Connnect Success!";
+                sw.WriteLine(welcome);
+                sw.Flush();
+
+                while (true)
+                {
+                    strMsg = sr.ReadLine();
+                    if (strMsg == "exit")  //exit 메시지 수신시 종료하기
+                        break;
+
+                    Console.WriteLine(strMsg);
+                    sw.WriteLine(strMsg);
+                    sw.Flush();
+                }
+
+                sw.Close();
+                sr.Close();
+                ns.Close();
+                sockServer.Stop();
+
+                Console.WriteLine("Client 연결 종료!");
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
